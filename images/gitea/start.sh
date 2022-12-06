179cbb25cd3da485fc72fc073296c47338379a6f
Michał Puchawski <michal.puchawski@ecom.software>
2022-12-06 20:46:14 +0100

    Check if users and auth already exists

--- images/gitea/start.sh
+++ images/gitea/start.sh
@@ -6,11 +6,7 @@ echo "Executing entrypoint"
 
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:3000)" != "200" ]]; do sleep 2; done'  
 
-echo "Create users"
-su -c 'gitea admin user create --admin --username ci_admin --password change_me --email ci_admin@localhost --must-change-password=true' git
-su -c 'gitea admin user create --username jenkins --password change_me --email jenkins@localhost --must-change-password=true' git
+su -c './install.sh' git
 
-echo "Add keycloak oauth"
-su -c 'gitea admin auth add-oauth --name keycloak --provider openidConnect --key gitea --secret ${GITEA_CLIENT_SECRET} --auto-discover-url http://keycloak:8080/keycloak/realms/ci_cd/.well-known/openid-configuration' git
 wait
 
