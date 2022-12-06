a0973190712a6af5c46a74302724d67b1c7c0af4
mpuch12 <45654602+mpuch12@users.noreply.github.com>
2022-12-06 20:49:48 +0100

    Check if users and auth already exists (#12)

---
+++ images/gitea/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+trap "{ echo 'Terminated with Ctrl+C'; }" SIGINT
+
+waitForSuccessfulConnection() {
+  local counter=1
+  local max_attempts=30
+
+  while :; do
+    echo "Attempt no: $counter/$max_attempts trying to connect with $1"
+    [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $1)" != "200" ]] || break
+    [[ "$counter" -lt "$max_attempts" ]] || break
+    sleep 2
+    counter=$((counter + 1))
+  done
+}
+
+waitForSuccessfulConnection "localhost:3000"
+
+
+is_ci_admin_user_exists=$(gitea admin user list | grep "ci_admin")
+
+if [[ -z "$is_ci_admin_user_exists" ]]; then
+  gitea admin user create --admin --username ci_admin --password change_me --email ci_admin@localhost --must-change-password=true
+else
+  echo "Skipped creating user ci_admin"
+fi
+
+is_jenkins_user_exists=$(gitea admin user list | grep "jenkins")
+
+if [[ -z "$is_jenkins_user_exists" ]]; then
+  gitea admin user create --username jenkins --password change_me --email jenkins@localhost --must-change-password=true
+else
+  echo "Skipped creating user jenkins"
+fi
+
+echo "Waiting for keycloak connection"
+waitForSuccessfulConnection "http://keycloak:8080/keycloak/realms/ci_cd/.well-known/openid-configuration"
+
+
+is_keycloak_auth_exists=$(gitea admin auth list | grep "keycloak")
+
+if [ -z "$is_keycloak_auth_exists" ]; then
+  gitea admin auth add-oauth \
+    --name keycloak \
+    --provider openidConnect \
+    --key gitea \
+    --secret ${GITEA_CLIENT_SECRET} \
+    --auto-discover-url http://keycloak:8080/keycloak/realms/ci_cd/.well-known/openid-configuration
+   echo "Keycloak oauth created successfully"
+else
+  echo "Skipped creating keycloak oauth"
+fi
