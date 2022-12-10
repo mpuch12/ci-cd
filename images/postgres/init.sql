08b465863e847c1ae0c62c6fe017ba74f8386fd0
Michał Puchawski <michal.puchawski@ecom.software>
2022-12-10 08:18:42 +0100

    Connect gitea to postgresql

--- images/postgres/init.sql
+++
@@ -1,5 +0,0 @@
-create user "keycloak-admin";
-alter role "keycloak-admin" CREATEROLE;
-alter user "keycloak-admin" with password 'Admin123';
-CREATE DATABASE keycloak;
-GRANT ALL PRIVILEGES ON DATABASE keycloak TO "keycloak-admin";
