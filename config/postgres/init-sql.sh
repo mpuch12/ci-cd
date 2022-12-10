a30b0f4a58e75654ea5cd7ab8afba1da199c31a8
Michał Puchawski <michal.puchawski@ecom.software>
2022-12-10 12:26:42 +0100

    Fix connect gitea to postgresql

--- config/postgres/init-sql.sh
+++ config/postgres/init-sql.sh
@@ -4,7 +4,7 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     create user "keycloak-admin";
     alter role "keycloak-admin" CREATEROLE;
-    alter user "keycloak-admin" with password '${JENKINS_ADMIN_PASSWORD}';
+    alter user "keycloak-admin" with password '${KEYCLOAK_ADMIN_PASSWORD}';
     CREATE DATABASE keycloak;
     GRANT ALL PRIVILEGES ON DATABASE keycloak TO "keycloak-admin";
 
