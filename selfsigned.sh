5797df2d0f03a352ed2b385a3e1bfde56f98290a
Michał Puchawski <michal.puchawski@ecom.software>
2022-12-11 12:51:59 +0100

    Refactor and cleanup configuration

--- selfsigned.sh
+++ selfsigned.sh
@@ -2,4 +2,4 @@
 
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
   -keyout example.key -out example.crt -subj "/CN=<your-domain>" \
-  -addext "subjectAltName=DNS:<yout-domain>"
+  -addext "subjectAltName=DNS:<your-domain>"
